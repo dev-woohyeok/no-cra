66b2b08c30504edb83b9c8f3cbad6223362c1371
dev_woo <mskim2525@naver.com>
2025-01-31 14:58:20 +0900

    Refactor: JSX 문법을 위한 Babel 트랜스파일 적용

---
+++ src/app.js
@@ -0,0 +1,5 @@
+const App = () => {
+	return React.createElement('h1', null, 'Hello, World!');
+};
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(React.createElement(App));
