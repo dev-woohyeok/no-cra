8c740f8e8bdb4581948e1a8ef3817e5363cf636e
dev_woo <mskim2525@naver.com>
2025-01-31 15:30:35 +0900

    feat: 환경변수 적용

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const App = () => {
-	return React.createElement('h1', null, 'Hello, World!');
+	// return React.createElement('h1', null, 'Hello, World!');
+	return <h1>{apiURL}</h1>;
 };
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(React.createElement(App));
+
+const apiURL = process.env.APP_API_URL;
